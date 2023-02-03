719fa974c80da4baa40dc3cbce85de5a7f025c38
marine-spaak <marine.spaak@gmail.com>
2023-02-03 16:56:34 +0100

    exercice 2 compteurs de clics YES NO, en utilisant current Target

--- js/exo2target.js
+++ js/exo2target.js
@@ -12,18 +12,25 @@ let compteur_yes = 0;
 let compteur_no = 0;
 
 // Je crée la fonction qui incrémente le compteur YES
-function handleClick_yes() {
-    compteur_yes++;
-    spanHTMLyes.textContent = compteur_yes;
-}
+function handleClick(event) {
 
-// Je crée la fonction qui incrémente le compteur NO
-function handleClick_no() {
+    const htmlButtonClicked = event.currentTarget;
+
+    // Je récupère l'id 
+    const idButton = htmlButtonClicked.id;
+
+    if (idButton == "yes") {
+        compteur_yes++;
+        spanHTMLyes.textContent = compteur_yes;
+    }
+
+    else {
     compteur_no++;
     spanHTMLno.textContent = compteur_no;
+    }
 }
 
 // Je n'oublie pas mon "écouteur"
 // à l'aide du addEventListener
-boutonHTMLyes.addEventListener('click', handleClick_yes);
-boutonHTMLno.addEventListener('click', handleClick_no);
+boutonHTMLyes.addEventListener('click', handleClick);
+boutonHTMLno.addEventListener('click', handleClick);
