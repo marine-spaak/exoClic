a55bec236c1601c38a1489af3dcb98431add42a6
marine-spaak <marine.spaak@gmail.com>
2023-02-03 16:41:58 +0100

    Exo 1 compteur de clic OK

--- js/exo1.js
+++ js/exo1.js
@@ -1,41 +1,19 @@
-/*
-    <button id="button">Clic !</button>
-    <div id="times">
-        On a cliqué
-        <span id="counter">0</span>
-        fois
-    </div>
-*/
+// Je sélectionne le bouton
+const boutonHTML = document.querySelector("#button");
 
-//- 1: stocker l'élement HTML bouton
-const htmlButton = document.querySelector("#button");
-//- 2: stocker l'élement HTML span
-const htmlSpan = document.querySelector("#counter");
+// Je sélectionne le span
+// C'est là où s'affichera le compteur
+const spanHTML = document.querySelector("#counter");
 
-// je définis le compteur
+// Je vais définir mon compteur
 let compteur = 0;
 
-/*-3: On défini la fonction :
-DEFINIR LES ACTIONS AU CLICK
-    - incrémenter un compteur
-    - on remplace la valeur dans le DOM 
-    (grâce à la variable 2)*/
-// Une petite précision : les fonctions avec addEventListener
-// portent par convention le mot "handle" avant le nom de la fonction
-// Un handler ne peut pas recevoir vos propres paramètres !
-
-// handleIncrement / handleCounterIncrement / handleClick
+// Je crée la fonction qui incrémente le compteur
 function handleClick() {
-    // Est-ce qu'on défini le compteur dans la fonction ? Oui/Non Pourquoi ?
-    console.log("click sur le bouton");
-    // incrémenter un compteur
-    compteur = compteur + 1;
-
-    // on remplace la valeur dans le DOM 
-    htmlSpan.textContent = compteur;
+    compteur = compteur++;
+    spanHTML.textContent(compteur);
 }
 
-/*-4: Liaison :
-<element HTML 1>.addEventListener
-  ('click', on appelle la fonction 3)*/
-htmlButton.addEventListener('click', handleClick);
+// Je n'oublie pas mon "écouteur"
+// à l'aide du addEventListener
+boutonHTML.addEventListener('click', handleClick);
